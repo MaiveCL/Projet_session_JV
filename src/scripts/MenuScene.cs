213e7d6ad3eae0f62de8f62afb6d15e1338d4078
MaiveCL <maive_@hotmail.com>
2025-12-03 19:44:27 -0500

    avancement du menu, aide terminée

--- src/scripts/MenuScene.cs
+++ src/scripts/MenuScene.cs
@@ -3,14 +3,38 @@ using System;
 
 public partial class MenuScene : Control
 {
+	// Références exportées pour assigner dans l'inspecteur
+	[Export] private Button startButton;
+	[Export] private Button aide;
+	[Export] private AcceptDialog dialog;
+
 	public override void _Ready()
 	{
-		GetNode<Button>("VBoxContainer/Button").Pressed += OnStartPressed;
+		// Start button
+		startButton.Pressed += OnStartPressed;
+
+		// Comment Jouer button
+		aide.Pressed += OnCommentJouerPressed;
 	}
 
 	private void OnStartPressed()
 	{
 		GetNode<MondeStateMachine>("/root/Main/MondeStateMachine")
 			.ChangeState("GameState");
+	}
+
+	private void OnCommentJouerPressed()
+	{
+		// Affiche le popup
+		dialog.Popup();
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		// Fermer le popup avec n'importe quelle touche
+		if (dialog.Visible && @event is InputEventKey keyEvent && keyEvent.Pressed)
+		{
+			dialog.Hide();
+		}
 	}
 }
